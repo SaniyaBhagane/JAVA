166be9ba97a3a77d30dcf749b75d7c6911e55271
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-07-27 23:40:02 +0530

    integer datatypes

--- Demo.java
+++ Demo.java
@@ -1,6 +1,11 @@
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello, Saniya!");
-        System.out.println("Java is working perfectly.");
+        // Integers --> byte, short, int, long
+        byte b = 5;
+        short s = 10;
+        int i = 4000;
+        long l = 100000;
+
+        System.out.println("Integer Data Types: " + b + ", " + s + ", " + i + ", " + l);
     }
 }
