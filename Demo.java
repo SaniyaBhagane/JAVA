fa1c0cf526fe0e5037bc8201c31d2a6879b0ba23
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-07-27 23:56:20 +0530

    conversion of hex

--- Demo.java
+++ Demo.java
@@ -2,15 +2,18 @@ public class Demo {
     public static void main(String[] args) {
         // Integers --> byte, short, int, long
         // binary(2), octal(8), decimal(10), hexadecimal(16)
-        // byte b = 5;
-        byte b = 0b101; // binary representation of 5
+        
+        byte b = 5;
+        // byte b = 0b101; // binary representation of 5
+        // byte b = 05; //octal representation of 5
+        // byte b = 0x5; // hexadecimal representation of 5 also written as 0XA
         short s = 10;
         int i = 4000;
         long l = 100000;
-
         // Real 
-        float f = 10.53f;
-        double d = 23.0987;
+        float f = 10.53f; // 32 bits single precision
+        double d = 23.0987; //64 bits double precision
+        // double d = 6.022e23; // 6.022 * 10^23 --> scientific notation
 
         // Characters
         char c = 'a'; //'a' --> int value of a = 97 --> binary value of 97 = 01100001 --> store 01100001 in memory
