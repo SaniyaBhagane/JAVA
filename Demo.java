5a3bd0dd49c32b3c506e34daf90c31140730e855
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-07-27 23:46:23 +0530

    char bollean types

--- Demo.java
+++ Demo.java
@@ -6,6 +6,20 @@ public class Demo {
         int i = 4000;
         long l = 100000;
 
+        // Real 
+        float f = 10.53f;
+        double d = 23.0987;
+
+        // Characters
+        char c = 'a'; //'a' --> int value of a = 97 --> binary value of 97 = 01100001 --> store 01100001 in memory
+
+        // Boolean
+        boolean bool = true;
+    
+
         System.out.println("Integer Data Types: " + b + ", " + s + ", " + i + ", " + l);
+        System.out.println("Real numberTypes: " + f + ", " + d );
+        System.out.println("Character: " + c);
+        System.out.println("Boolean: " + bool);
     }
 }
