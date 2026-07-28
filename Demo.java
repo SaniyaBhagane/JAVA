280a23d127925d475298830a6b372f0305d8f668
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-07-28 16:38:26 +0530

    rrelational operators

--- Demo.java
+++ Demo.java
@@ -74,39 +74,53 @@ public class Demo {
 
         //-----------OPERATORS in JAVA--------
         // Arithmatic Operators --> +, -, *, /, %, +=, -=, *=, /=, %=, ++, --
-        int a = 5;
-        int b = 10;
-        int c = a + b; // 15
-        int d = a - b; // -5
-        int e = a * b; // 50
-        int f = b / a; // 2
-        int g = b % a; // 0
-        System.out.println(c + " , " + d + " , " + e + " , " + f + " , " + g);
+        // int a = 5;
+        // int b = 10;
+        // int c = a + b; // 15
+        // int d = a - b; // -5
+        // int e = a * b; // 50
+        // int f = b / a; // 2
+        // int g = b % a; // 0
+        // System.out.println(c + " , " + d + " , " + e + " , " + f + " , " + g);
 
-        int h = a + 2;
-        //h = h + 2; // Since this is okay in CSE
-        h += 2; // h = h + 2; // h = 9
-        h -= 2; // h = h - 2; // 7
-        h *= 3; // h = h * 3;
-        h /= 5; // h = h / 5;
-        h %= 5; // h = h % 5;
-        System.out.println(h); 
+        // int h = a + 2;
+        // //h = h + 2; // Since this is okay in CSE
+        // h += 2; // h = h + 2; // h = 9
+        // h -= 2; // h = h - 2; // 7
+        // h *= 3; // h = h * 3;
+        // h /= 5; // h = h / 5;
+        // h %= 5; // h = h % 5;
+        // System.out.println(h); 
         
-        int i = 6;
-        i++; // i = i + 1 --> i += 1; 
-        i--; // i = i - 1 --> i -= 1;
-        System.out.println(i); // i = 7;
+        // int i = 6;
+        // i++; // i = i + 1 --> i += 1; 
+        // i--; // i = i - 1 --> i -= 1;
+        // System.out.println(i); // i = 7;
 
-        // pre increment and post increment/decrement.
-        int j = 7;
-        j++;  //postfix increment
-        ++j; // prefix increment
+        // // pre increment and post increment/decrement.
+        // int j = 7;
+        // j++;  //postfix increment
+        // ++j; // prefix increment
     
-        // j = 9
-        int k = j++; // k = j; j = j + 1;
-        System.out.println(j + " , " + k); // 10, 9
-        int l = ++j; // l = j + 1;
-        System.out.println(j + " , " + l); // 11, 11
+        // // j = 9
+        // int k = j++; // k = j; j = j + 1;
+        // System.out.println(j + " , " + k); // 10, 9
+        // int l = ++j; // l = j + 1;
+        // System.out.println(j + " , " + l); // 11, 11
 
+        // Relational Operators. -> ==, !=, <, > , <=, >=
+        // int a = 10;
+        // int b = 10;
+        // boolean c = (a == b);
+        // System.out.println(c); // true
+        // boolean d = (a != b);
+        // System.out.println(d); // false
+
+        // boolean e = (a < b); // false
+        // boolean f = (a <= b); // true
+        // System.out.println(e);
+        // System.out.println(f);
+
+        
     }
 }
