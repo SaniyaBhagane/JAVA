dfa18934f4b7b3c6b1b29b5f19d5e1cebde41393
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-07-30 16:37:56 +0530

    Q4

--- DEMO2.java
+++ DEMO2.java
@@ -61,6 +61,18 @@ public class DEMO2{
         // }
         // sc.close();
 
+        // Q4. Greater no
+        // Scanner sc = new Scanner(System.in);
+        // int num1, num2;
+        // System.out.print("Enter first number: ");
+        // num1 = sc.nextInt();
+        // System.out.print("Enter second number: ");
+        // num2 = sc.nextInt();
+        // if(num1 > num2 ){
+        //     System.out.println("The first number is greater.");
+        // }else{
+        //     System.out.println("The second number is greater.");
+        // }sc.close();
         
     }
 }
