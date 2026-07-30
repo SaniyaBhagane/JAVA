c5f1cec548f37564face0b48e4c8a07651788c12
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-07-30 18:36:42 +0530

    que done

--- DEMO2.java
+++ DEMO2.java
@@ -146,15 +146,15 @@ public class DEMO2{
         // }
 
         // Q10. leap year check
-            int year;  
-            Scanner sc = new Scanner(System.in);
-            System.out.print("Enter a year: ");
-            year = sc.nextInt();
-            if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
-                System.out.println(year + " is a leap year");
-            }else{
-                System.out.println(year + " is not a leap year.");
-            }sc.close();
+            // int year;  
+            // Scanner sc = new Scanner(System.in);
+            // System.out.print("Enter a year: ");
+            // year = sc.nextInt();
+            // if(year % 4 == 0 && year % 100 != 0 || year % 400 == 0){
+            //     System.out.println(year + " is a leap year");
+            // }else{
+            //     System.out.println(year + " is not a leap year.");
+            // }sc.close();
 
     
     }
