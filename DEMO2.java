5cfa62f412534a803b116703454374eb3c14c349
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-07-30 16:30:45 +0530

    Q1, 2

--- DEMO2.java
+++ DEMO2.java
@@ -1,18 +1,53 @@
-public class DEMO2 {
-    public static void main(String[] args) {
-        byte b = 42;
-        char c = 'a';
-        short s = 1024;
-        int i = 50000;
-        float f = 5.67f;
-        double d =  .1234;
 
-        double result = (f * b) + (i / c) - (d * s);
-        // f * b --> float
-        // i / c --> integer
-        // d * s --> double
-        // float + integer --> float - double --> double
-        System.out.println((f * b) +  " + " + (i / c) + " - " +(d * s)); //238.14 + 515 - 126.3616
-        System.out.println("result = " + result ); //result = 626.7784146484375
-    }
+import java.util.Scanner;
+
+// public class DEMO2 {
+//     public static void main(String[] args) {
+//         byte b = 42;
+//         char c = 'a';
+//         short s = 1024;
+//         int i = 50000;
+//         float f = 5.67f;
+//         double d =  .1234;
+
+//         double result = (f * b) + (i / c) - (d * s);
+//         // f * b --> float
+//         // i / c --> integer
+//         // d * s --> double
+//         // float + integer --> float - double --> double
+//         System.out.println((f * b) +  " + " + (i / c) + " - " +(d * s)); //238.14 + 515 - 126.3616
+//         System.out.println("result = " + result ); //result = 626.7784146484375
+//     }
+// }
+
+
+public class DEMO2{
+    public static void main(String[] args){
+        // Q1. positive no check
+        // int num;
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter a number: ");
+        // num = sc.nextInt();
+        // if(num > 0){
+        //     System.out.println(num + " is a positive number");
+        // }else if(num < 0){
+        //     System.out.println(num + " is a negative number");
+        // }else{
+        //     System.out.println(num + " is zero");
+        // }
+        // sc.close();
+
+    // Q2. EVEN ODD no check
+    //     int num;
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.print("Enter a number: ");
+    //     num = sc.nextInt();
+    //     if(num % 2 == 0 ){
+    //         System.out.println(num + " is a even number");
+    //     }else{
+    //         System.out.println(num + " is a odd number");
+    //     }
+    //     sc.close();
+
+    // }
 }
