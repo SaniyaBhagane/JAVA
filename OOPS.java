abff7454dbec66ef09ceee8601568434d20405a1
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-08-03 13:01:07 +0530

    call by value

--- OOPS.java
+++ OOPS.java
@@ -152,6 +152,22 @@ String --> null (nothing)
 
 //         System.out.println("I am in fifth constructor");
 //     }
+ 
+    //call by value
+        public static void main(String[] args) {
+        int x = 4;
+        int y = 5;
 
-    
+        System.out.println(x + " , " + y);
+
+        addTen(x, y);
+        
+        System.out.println(x + " , " + y);
+
+    }
+    static void addTen(int x, int y) {
+        x = x + 10;
+        y = y + 10;
+    }
+
 }
