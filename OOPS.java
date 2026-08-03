f1591ff40f9309f1bb6add2eabf0341a819ee9e8
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-08-03 18:32:10 +0530

    ENCAPSULATION

--- OOPS.java
+++ OOPS.java
@@ -246,13 +246,13 @@ String --> null (nothing)
 //     }
 
     //use of String[]args
-    public static void main(String[] args) {
-        System.out.println("Number of arguments are " + args.length);
-        for(int i=0; i<args.length; i++) {
-            System.out.println("Argument " + i + " = " + args[i]);
-        }
+    // public static void main(String[] args) {
+    //     System.out.println("Number of arguments are " + args.length);
+    //     for(int i=0; i<args.length; i++) {
+    //         System.out.println("Argument " + i + " = " + args[i]);
+    //     }
 
         //java Demo5 input.txt output.txt
         //java Demo5 
-    }
+    // }
 }
