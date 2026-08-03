f1c67ae685627d0fe227f6d90655ea432de70733
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-08-03 14:22:36 +0530

    static keyword

--- OOPS.java
+++ OOPS.java
@@ -195,6 +195,33 @@ String --> null (nothing)
 //         this.y = r.y;
     // }
 
+    //static keyword
+//      public static void main(String[] args) {
+//         Student s1 = new Student("Aditya", 28, 101);
+//         Student s2 = new Student("Rohit", 28, 102);
 
-    
+//         //Student.college = "IIT Guwahati";
+
+//         System.out.println(s1.name + " , " + s1.age + " , " + s1.rollNumber + " , " + Student.college);
+//         System.out.println(s2.name + " , " + s2.age + " , " + s2.rollNumber + " , " + Student.college);
+//     }
+// }
+// class Student {
+//     String name;
+//     int age;
+//     int rollNumber;
+//     static String college = "IIT Guwahati";
+//     static int grade;
+
+//     Student(String name, int age, int rollNumber) {
+//         this.name = name;
+//         this.age = age;
+//         this.rollNumber = rollNumber;
+//     }
+
+//     // Static block;
+//     static {
+//         grade = 8;
+//     }
+
 }
