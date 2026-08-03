75d010e7d07008a97513c7b67f8be221a17879be
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-08-03 14:29:50 +0530

    final keyword

--- OOPS.java
+++ OOPS.java
@@ -224,4 +224,25 @@ String --> null (nothing)
 //         grade = 8;
 //     }
 
+    // //final keyword
+    // public static void main(String[] args) {
+    //     Random r1 = new Random();
+    //     System.out.println(r1.PI);
+    //     final int x;
+    //     x = 4;
+    //     System.out.println(x);
+//     }
+// }
+
+//why main is static in java ??
+// Demo4 d1 = new Demo4();
+// d1.main()
+// Demo4.main()
+
+// class Random {
+//     static final double PI;
+//     static {
+//         PI = 3.14;
+//     }
+
 }
