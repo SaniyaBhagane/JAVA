b6e2750beddb74d7e74717457058524fc09567f6
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-08-04 19:12:35 +0530

    inner class

--- opps2.java
+++ opps2.java
@@ -386,61 +386,75 @@ Multiple
 //6. Can abstract classes have no abstract method ? --> Yes
 
 //NESTED CLASSES
-public class opps2{
-    public static void main(String[] args) {
-
-        Outer outer = new Outer();
-
-        Outer.Inner inner = new Outer.Inner(outer);
-        inner.fun();
-    }
-}
 
 // Static nested class
-class Outer {
-
-    private static int x = 4;
-    int y;
-
-    static class Inner {
-
-        Outer outer;
-        private String name;
-        private static int number;
-
-        Inner(Outer outer) {
-            this.outer = outer;
-        }
-
-        void fun() {
-            System.out.println(x);
-            System.out.println(outer.y);
-        }
-
-        static void fun2() {
-
-        }
-    }
-}
-
-class BankAccount {
-
-    private static class InterestCalculator {
-        static double calculateYearly(double principal, double rate) {
-            return principal * rate;
-        }
-    }
-
-    public double computeIntersest(double principal) {
-        return InterestCalculator.calculateYearly(principal, 0.09);
-    } 
-}
-
+// public class opps2{
+//     public static void main(String[] args) {
+//         Outer outer = new Outer();
+//         Outer.Inner inner = new Outer.Inner(outer);
+//         inner.fun();
+//     }
+// }
+// class Outer {
+//     private static int x = 4;
+//     int y;
+//     static class Inner {
+//         Outer outer;
+//         private String name;
+//         private static int number;
+//         Inner(Outer outer) {
+//             this.outer = outer;
+//         }
+//         void fun() {
+//             System.out.println(x);
+//             System.out.println(outer.y);
+//         }
+//         static void fun2() {
+//         }
+//     }
+// }
+// class BankAccount {
+//     private static class InterestCalculator {
+//         static double calculateYearly(double principal, double rate) {
+//             return principal * rate;
+//         }
+//     }
+//     public double computeIntersest(double principal) {
+//         return InterestCalculator.calculateYearly(principal, 0.09);
+//     } 
+// }
 // Use cases of static nested classes :
 /*
  1. As helper class for any outer class.
  2. Builder Design pattern
  3. If you want to have static methods inside a nested class.
  4. Request/Response DTO
-
 */
+
+//INNER CLASS
+// public class opps2{
+//     public static void main(String[] args) {
+//         Outer outer = new Outer();
+//         //Outer.Inner inner = new Outer.Inner();
+//         Outer.Inner inner =  outer.new Inner();
+//        // Outer.Inner inner = new Outer().new Inner();
+//         inner.fun();
+//         inner.fun2();
+//         Outer.Inner.fun2();
+//     }
+// }
+// class Outer {
+//     int x = 10;
+//     class Inner {
+//         //int x = 20;
+//         //Outer outer;
+//         static int x = 10;
+//         void fun() {
+//             System.out.println(x);
+//             //System.out.println(Outer.this.x);
+//         }
+//         static void fun2() {
+//             System.out.println("Hello");
+//         }
+//     }
+// }
