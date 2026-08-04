a43e14ed0e918cf2e9f49283e8e6614ea7340e0e
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-08-04 19:34:35 +0530

    anonymous class

--- opps2.java
+++ opps2.java
@@ -510,3 +510,50 @@ If
 Loop
 Static block
 */
+
+// ANONYMOUS CLASSS
+// public class opps2{
+//     public static void main(String[] args) {
+//         // Person p1 = new Person();
+//         // p1.introduce();
+//         // Person p2 = new Guest();
+//         // p2.introduce();
+//         Person p2 = new Person() {
+//             String name = "Aditya";
+//             @Override
+//             void introduce() {
+//                 greet();
+//                 System.out.println("Hi, I am " + name);
+//             }
+//             void greet() {
+//                   System.out.println("Hello");
+//             }
+//         };
+//         p2.introduce();
+//     }
+// }
+// class Person {
+//     Person() {
+//     }
+//     void introduce() {
+//         System.out.println("Hi, I am a person");
+//     }
+// }
+// class Guest extends Person {
+//     @Override
+//     void introduce() {
+//         System.out.println("Hi, I am a Guest");
+//     }
+// }
+
+/*
+Static Nested class
+
+Inner Class
+
+Anonymous class --> Lambdas
+
+Local class
+
+*/
+
