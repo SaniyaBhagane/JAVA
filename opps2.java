79eaf3328ecc3c7b47b95582737b7394363c5380
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-08-06 22:52:43 +0530

    toString

--- opps2.java
+++ opps2.java
@@ -673,3 +673,23 @@ Local class
 //     }
 // }
 
+//---------------OBJECT CLASS ----------------
+// tostring()
+// import java.util.objects;
+public class opps2{
+    public static void main(String[] args){
+        Student s1 = new Student();
+        s1.name = "Aditya";
+        s1.age = 28;
+        // System.out.println(s1.toString());
+        System.out.println(s1);
+    }
+}
+class Student{
+    String name;
+    int age;
+    @Override
+    public String toString(){
+        return (name + ", " + age);
+    }
+}
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,5 +1,4 @@
-n(String[] args) {
-        System.out.println("Number of arguments are " + args.length);
-        for(int i=0; i<args.length; i++) {
-            System.out.println("Argument " + i + " = " + args[i]);
-        }
+ide
+    public String toString(){
+        return (name + ", " + age);
+    }
