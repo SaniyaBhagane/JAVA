d17a65fc7ea46eb03e74577a17969e33d7695789
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-08-07 23:21:52 +0530

    problem of immutability

--- opps2.java
+++ opps2.java
@@ -1096,3 +1096,14 @@ Problems with this approach :
 // // String pool -> "Hello, " World", "Hello World"
 // // Heap --> "Hello World"
 
+// public class opps2{
+//     public static void main(String[] args) {
+//         String s = "";
+//         for(int i=0; i<5; i++) {
+//             s += i; // s = s + i;
+//             System.out.println(s);
+//         }
+//     }
+// }
+// Problem of Immutability
+// "0" -> "01" --> "012" -> "0123" -> "01234"
