0a70e08fd2d82109ed75d922869c72333275a7b0
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-08-07 16:12:08 +0530

    enums problem

--- opps2.java
+++ opps2.java
@@ -741,3 +741,47 @@ Local class
 //         return super.clone();
 //     }
 // }
+
+//----------ENUMS--------------------
+public class opps2{
+    public static void main(String[] args) {
+        // int status = PaymentStatus.SUCCESS;
+
+        // System.out.println(status);
+
+        // int status2 = 100;
+
+        // if(status == Role.ADMIN) {
+
+        // }
+
+        String status = PaymentStatus.SUCCESS;
+        System.out.println(status);
+
+        if(status == "success") {
+
+        }
+    }
+}
+
+// payment status --> success, failed, pending
+// final
+
+/*
+Problems with this approach :
+1. Type safety
+2. Poor Readability
+3. No Grouping od related enitites
+*/
+
+class PaymentStatus {
+    public static final String SUCCESS = "Success";
+    public static final String FAILED = "Failed";
+    public static final String PENDING = "Pending";
+}
+
+class Role {
+    public static final int USER = 1;
+    public static final int ADMIN = 2;
+    public static final int MANAGER = 2;
+}
