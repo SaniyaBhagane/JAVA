8adcb19e08b2fb4b4d313d6bade712c58d65d80c
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-08-08 12:08:55 +0530

    string methods

--- opps2.java
+++ opps2.java
@@ -1107,3 +1107,48 @@ Problems with this approach :
 // }
 // Problem of Immutability
 // "0" -> "01" --> "012" -> "0123" -> "01234"
+
+//STRING METHODS
+public class opps2{
+    public static void main(String[] args) {
+        String s1 = new String();
+
+        // String
+        String s2 = new String("Hello");
+
+        String s3 = "Aditya";
+        String s4 = new String(s3);
+
+        // char array
+        char[] arr = {'A', 'd', 'i', 't', 'y', 'a', ' ', 'T', 'a', 'n', 'd', 'o', 'n'};
+        String s5 = new String(arr); 
+        //arr[0] = 'B';
+
+       // System.out.println(s5);
+
+        // char array subset
+        String s6 = new String(arr, 0, 6);
+       // System.out.println(s6);
+
+        byte[] arr2 = {97, 98, 99};
+        String s7 = new String(arr2, 0, 2);
+
+        //System.out.println(s7);
+
+        // StringBuilder / StringBuffer
+        StringBuffer sb = new StringBuffer("Hello");
+        String s8 = new String(sb);
+
+        System.out.println(s8);
+    }
+}
+
+/*
+length/Emptiness --> length(), isEmpty(), isBlank()
+charater access --> charAt(), toCharArray()
+comparison --> equals(), equalsIgnoreCase(), compareTo()
+searching --> contains(), startsWith(), endsWith(), indexOf(), lastIndexOf()
+extracion/ Transformation --> substring(), repeat(), toUpperCase(), toLoweCase(), trim(), strip(), stripLeading(), stripTrailing(), replace(), replaceAll(), replaceFirst(), split(), join()
+conversion --> valueOf(), toString(), getBytes()
+advanced --> intern(), format(), matches(), regionMatches(), codePointAt(), codePointBefore(), codePointCount(), offsetByCodePoints()
+ */
