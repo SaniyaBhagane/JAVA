150a4057f8e9bfcec65ceca1988ac02fe5a84d72
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-08-09 18:32:21 +0530

    bounds of generics

--- opps2.java
+++ opps2.java
@@ -1363,3 +1363,21 @@ Heap:
 // // Generic Methods
 // // <T> returnType methodName(T parameter) {
 // // }
+
+//BOUNDS IN GENERICS
+// public class opps2{
+//         public static void main(String[] args) {
+//         Box<Integer> b1 = new Box<>();
+//         b1.value = 5;
+//         b1.printDouble();
+//     }
+// }
+// // Generics --> T can be anything
+// // Bounds in Generics
+// // Upper bound --> T is atleast Number or its subtype
+// class Box<T extends Number> {
+//     T value;
+//     public void printDouble() {
+//         System.out.println(value.doubleValue());
+//     }
+// }
