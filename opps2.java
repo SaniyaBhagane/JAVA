bab3d0217f3a930b70479379fac9c6d0dd788bb1
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-08-10 16:20:34 +0530

    enhanced for loop

--- opps2.java
+++ opps2.java
@@ -1577,4 +1577,39 @@ import java.util.*;
 // Iterable
 // 10 -> 20 -> 30 -> 40 -> 50
 
-public 
+// public class opps2{
+//         public static void main(String[] args) {
+//         String[] names = {"Aditya", "Rohit", "Rohan", "Monu"};
+//         NameContainer container = new NameContainer(names);
+//         // Iterator<String> it = container.iterator();
+//         // while(it.hasNext()) {
+//         //     System.out.println(it.next());
+//         // }
+//         // Enhanced for loop
+//         for(String name : container) {
+//             System.out.println(name);
+//         }   
+//     }
+// }
+// class NameContainer implements Iterable<String> {
+//     private String[] names;
+//     private int size;
+//     NameContainer(String[] names) {
+//         this.names = names;
+//         this.size = this.names.length;
+//     }
+//     @Override
+//     public Iterator<String> iterator() {
+//         return new Iterator<String>() {
+//             private int pos = 0;
+//             @Override
+//             public boolean hasNext() {
+//                 return pos < size;
+//             }
+//             @Override
+//             public String next() {
+//                 return names[pos++];
+//             }
+//         };
+//     }
+// }
