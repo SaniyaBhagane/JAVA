1e9afadae1c78e89d65163ad8ef4abd436eb4b12
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-08-10 19:30:06 +0530

    methods()

--- opps2.java
+++ opps2.java
@@ -1705,23 +1705,40 @@ import java.util.*;
 //         list.add(1);
 //         list.add(2);
 //         list.add(3);
+
         //System.out.println(list.get(1));
+
         //list.set(1, 5);
+
         //list.addAll(0, List.of(9,8,7));
+
         //System.out.println(list);
+
         //list.remove(0);
+
         //System.out.println(list);
+
         // list.indexOf(2);
         // list.lastIndexOf(5);
+
         // ListIterator<Integer> it = list.listIterator(3);
+
         // while(it.hasPrevious()) {
         //     System.out.println(it.previousIndex());
         //     it.previous();
         // }
+
+        // while(it.hasNext()) {
+        //     System.out.println(it.nextIndex());
+        //     it.next();
+        // }
+
 //         List<Integer> l = List.of(1,2,3,4,5,6,7,8);
 //         //l.add(9);
+
 //         List<Integer> l2 = List.copyOf(l);
 //         l2.add(7);
+
 //         System.out.println(l2);
 //     }
 // }
