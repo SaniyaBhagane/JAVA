3d7298f251b9f5d877ec6a727f6dc12e7d8a8220
SaniyaBhagane <120231995+SaniyaBhagane@users.noreply.github.com>
2026-08-10 16:14:29 +0530

    iterqtor

--- opps2.java
+++ opps2.java
@@ -1556,3 +1556,25 @@ Heap:
 // }
 // class Cat extends Animal {
 // }
+
+//----------------------------ITERATOR------------------------
+import java.util.*;
+
+// public class opps2{
+//         public static void main(String[] args) {
+//         Collection<Integer> c = new TreeSet<>();
+//         c.add(10);
+//         c.add(20);
+//         c.add(30);
+//         c.add(40);
+//         c.add(50);
+//         Iterator<Integer> it =  c.iterator();
+//         while(it.hasNext()) {
+//             System.out.println(it.next());
+//         }
+//     }
+// }
+// Iterable
+// 10 -> 20 -> 30 -> 40 -> 50
+
+public 
